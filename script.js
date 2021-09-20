97c789742c04a404e115003879a0a410ffd54eef
Lucia Kokesova <lkokesova@gmail.com>
2021-09-20 21:17:07 +0200

    Fix previously commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function showAndHide(inv) {
     var innerStyle = document.getElementById(inv);
     console.log(innerStyle);
@@ -6,13 +5,5 @@ function showAndHide(inv) {
         innerStyle.style.display = 'block';
     } else {
         innerStyle.style.display = 'none';
-=======
-let x = document.getElementById('invention1');
-function ShowAndHide() {
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
->>>>>>> 3f6cd2a419e45264a18470f49260429af5a377ff
     }
 }
