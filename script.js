90815aecb1fc7102ea32c189b2e065b81a1390ce
Lucia Kokesova <lkokesova@gmail.com>
2021-10-11 20:36:08 +0200

    Scroll back to top

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ function showAndHide(inv) {
         innerStyle.style.display = 'none';
     }
 }
+
+var backToTop = $('<a>', {
+    href:'#',
+    class: 'back-to-top',
+    html: '<i class="fas fa-chevron-up fa-2x"></i>'
+});
+
+backToTop
+.hide()
+.appendTo('body')
+.on('click', function(){
+    $('body').animate({scrollTop: 0});
+});
+
+var win = $(window);
+win.on('scroll', function(){
+    if (win.scrollTop() >=500) backToTop.fadeIn();
+    else backToTop.hide();
+})
